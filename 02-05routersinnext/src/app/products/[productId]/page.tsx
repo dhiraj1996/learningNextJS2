ebe4e61441a0a4938d821582ab161e8d0a105d3c
dhiraj1996 <dhirajkumar1996@gmail.com>
2024-09-03 23:10:41 +0530

    third page in blog

--- 02-05routersinnext/src/app/products/[productId]/page.tsx
+++ 02-05routersinnext/src/app/products/[productId]/page.tsx
@@ -1,5 +1,17 @@
+import { Metadata } from "next";
 
+type Props = {
+    params: {
+        productId: String;
+    };
+};
 
-export default function ProductId({params}:{params: {productId: String};}) {
+export const generateMetadata = ({params}: Props) : Metadata => {
+    return{
+        title: `Product ${params.productId}`,
+    }
+}
+
+export default function ProductId({params}: Props) {
     return <h1> Product with id is {params.productId} </h1>
 }
